2baf6a0e74f7369027607d3c1c420ca7545ac695
Yamangulov Andrey <yamangulov@gmail.com>
2021-05-30 20:58:24 +0300

    Задания к уроку 5, задача 2, не подхватываются настройки из bootstrap.yml

--- 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
+++ 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
@@ -2,12 +2,13 @@ package ru.skillbox.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
