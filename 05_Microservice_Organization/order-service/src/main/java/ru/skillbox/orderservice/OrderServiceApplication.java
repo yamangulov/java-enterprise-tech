6daa98f0a9e201105f6f7eb27057bf03456279f3
Yamangulov Andrey <yamangulov@gmail.com>
2021-06-09 22:12:06 +0300

    Задания к уроку 5, задача 2 выполнена, на проверку

--- 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
+++ 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
@@ -2,13 +2,14 @@ package ru.skillbox.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories
+@RefreshScope
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
