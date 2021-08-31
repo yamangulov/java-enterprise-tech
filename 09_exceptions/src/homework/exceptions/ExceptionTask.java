060c25fe409e3386463034f03bec2c83fdae2d9e
Yamangulov Andrey <yamangulov@gmail.com>
2021-09-01 00:45:17 +0300

    Задания к уроку 9 выполнены, на проверку

--- 09_exceptions/src/homework/exceptions/ExceptionTask.java
+++ 09_exceptions/src/homework/exceptions/ExceptionTask.java
@@ -1,30 +1,49 @@
 package homework.exceptions;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class ExceptionTask {
 
     public static Optional<String> mergeStrings(String first, String second) {
-        return Optional.of(first.length() > second.length() ? first + second : second + first);
+        if ((first == null && second == null) || (Objects.equals(first, "") && Objects.equals(second, ""))) {
+            return Optional.empty();
+        } else {
+            String frst = Optional.of(first != null ? first : "").get();
+            String scnd = Optional.of(second != null ? second : "").get();
+            return Optional.of(frst.length() > scnd.length() ? frst + scnd : scnd + frst);
+        }
     }
 
-    public static long getPower(int n, int p) throws Exception {
+    public static long getPower(int n, int p) {
         if (n < 0 || p < 0) {
-            throw new Exception();
-        } else if (n == 0 && p == 0) {
-            throw new Exception();
+            return -1L;
+        } else if (n == 0) {
+            return -1L;
         } else {
             return (long) (Math.pow(n, p));
         }
     }
 
-    public static void customException(int a) {
-        throwUncheckedException();
+    public static void customException(int a) throws InvalidZeroParameterException {
+        if (a == 0) {
+            throw new InvalidZeroParameterException();
+        } else {
+            try {
+                throwUncheckedException();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void exceptionProcessing() throws Exception {
-        throwCheckedException();
-        throwUncheckedException();
+    public static void exceptionProcessing() {
+        try {
+            throwCheckedException();
+            throwUncheckedException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void throwCheckedException() throws Exception {
